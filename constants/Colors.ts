053043c1a913518699e8b37f209a9cc19d06a749
Rick Alves <henryworkdev.ti@gmail.com>
2025-03-03 15:56:42 -0300

    adicionando styles

---
+++ constants/Colors.ts
@@ -0,0 +1,13 @@
+const tintColorLight = '#0a7ea4';
+const tintColorDark = '#fff';
+
+export const Colors = {
+    primary: '#007AFF',  // Azul principal
+    secondary: '#34C759', // Verde secundário
+    background: '#F5F5F5', // Cinza claro de fundo
+    text: '#333', // Cor do texto principal
+    error: '#FF3B30', // Vermelho para erros
+    white: '#FFFFFF',
+    black: '#000000',
+  };
+  
---
+++ constants/Styles.ts
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from './Colors';
+
+export const Styles =  StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+    padding: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.text,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  titleInput: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: Colors.text,
+    marginBottom: 4,
+    textAlign: 'left',
+    width:'100%',
+  },
+  input: {
+    borderWidth:1,
+    borderRadius:8,
+    borderColor: Colors.primary,
+    marginBottom: 15,
+    padding: 10,
+    width: '100%',
+  },
+  inputError: {
+    borderColor: Colors.error,
+  },
+  button: {
+    backgroundColor: Colors.primary,
+    padding: 12,
+    borderRadius: 5,
+    width: '100%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: Colors.white,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  errorText: {
+    color: Colors.error,
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+});
